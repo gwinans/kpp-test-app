b392c76e1934281a794850169f91ccb567217a16
Geoff W <gwinans@gmail.com>
2025-08-19 19:41:38 -0700

    Update migrations to use alterTableWithBuilder

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,9 @@ module.exports = {
     client: 'mysql2',
     connection: {
       host: process.env.DB_HOST || '127.0.0.1',
-      user: process.env.DB_USER || 'testuser',
-      password: process.env.DB_PASSWORD || 'password',
-      database: process.env.DB_NAME || 'test_db'
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || 'letmein',
+      database: process.env.DB_NAME || 'ptosc'
     },
     migrations: {
       directory: './migrations'
--- migrations/20240101000001_add_json_test_column.js
+++ migrations/20240101000001_add_json_test_column.js
@@ -1,14 +1,25 @@
 /**
  * Add json_test column to test_table.
  */
+
+const { alterTableWithBuilder } = require('knex-ptosc-plugin');
+
 exports.up = function (knex) {
-  return knex.schema.alterTable('test_table', (table) => {
+  return alterTableWithBuilder(knex, 'test_table', (table) => {
     table.json('json_test');
+  }, {
+    maxLoad: 150,
+    criticalLoad: 50,
+    alterForeignKeysMethod: 'auto',
   });
 };
 
 exports.down = function (knex) {
-  return knex.schema.alterTable('test_table', (table) => {
+  return alterTableWithBuilder(knex, 'test_table', (table) => {
     table.dropColumn('json_test');
+  }, {
+    maxLoad: 150,
+    criticalLoad: 50,
+    alterForeignKeysMethod: 'auto',
   });
 };
