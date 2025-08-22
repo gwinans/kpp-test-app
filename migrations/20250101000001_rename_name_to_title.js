61056f698b4beb523fdbb5d6eb32a5a4f37b53d7
Geoff W <gwinans@gmail.com>
2025-08-21 21:00:00 -0700

    .

--- migrations/20250101000001_rename_name_to_title.js
+++ migrations/20250101000001_rename_name_to_title.js
@@ -1,16 +1,15 @@
 /**
  * Rename the name column to title in widgets.
  */
-const { alterTableWithPtosc } = require('knex-ptosc-plugin');
 
 exports.up = function (knex) {
-  return alterTableWithPtosc(knex, 'widgets', (table) => {
+  return knex.schema.alterTable('widgets', function(table) {
     table.renameColumn('name', 'title');
   });
 };
 
 exports.down = function (knex) {
-  return alterTableWithPtosc(knex, 'widgets', (table) => {
+  return knex.schema.alterTable('widgets', function(table) {
     table.renameColumn('title', 'name');
   });
 };
