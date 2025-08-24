cdfda17eb8b3edd995c16e110eafa8f05b1a781c
Geoff W. <gwinans@gmail.com>
2025-08-23 20:31:26 -0700

    .

--- kpp-reset.sh
+++ kpp-reset.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 MYSQL_PWD=test mysql -uroot ptosc -e "DROP TABLE IF EXISTS gadgets; DROP TABLE IF EXISTS widgets; TRUNCATE TABLE knex_migrations; UPDATE knex_migrations_lock SET is_locked = 0;"
+rm -rf node_modules/knex-ptosc-plugin
+mkdir node_modules/knex-ptosc-plugin
+cp -r ../knex-ptosc-plugin/* node_modules/knex-ptosc-plugin/
