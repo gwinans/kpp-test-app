835034c3b993942b6c7e088b4e0422053c4b9da0
Geoff W. <gwinans@gmail.com>
2025-08-23 18:04:50 -0700

    .

--- kpp-reset.sh
+++ kpp-reset.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-MYSQL_PWD=test mysql -uroot ptosc -e "DROP TABLE IF EXISTS gadgets; DROP TABLE IF EXISTS widgets; TRUNCATE TABLE knex_migrations;"
+MYSQL_PWD=test mysql -uroot ptosc -e "DROP TABLE IF EXISTS gadgets; DROP TABLE IF EXISTS widgets; TRUNCATE TABLE knex_migrations; UPDATE knex_migrations_lock SET is_locked = 0;"
