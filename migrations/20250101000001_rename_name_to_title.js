e3a964e7d8ba89c57ffc54688d30e0d647e26f5a
Geoff W. <awpti@awpti.org>
2025-10-05 21:23:39 -0700

    Use raw ptosc rename migrations

--- migrations/20250101000001_rename_name_to_title.js
+++ migrations/20250101000001_rename_name_to_title.js
@@ -2,27 +2,21 @@
  * Rename the name column to title in widgets.
  */
 
-const { alterTableWithPtosc } = require('knex-ptosc-plugin');
+const { alterTableWithPtoscRaw } = require('knex-ptosc-plugin');
 const { createPtoscOptions } = require('../src/ptosc-options');
 
 exports.up = function (knex) {
-  return alterTableWithPtosc(
+  return alterTableWithPtoscRaw(
     knex,
-    'widgets',
-    (table) => {
-      table.renameColumn('name', 'title');
-    },
+    'ALTER TABLE widgets CHANGE COLUMN `name` `title` VARCHAR(255) NOT NULL',
     createPtoscOptions('rename_widgets_name_to_title')
   );
 };
 
 exports.down = function (knex) {
-  return alterTableWithPtosc(
+  return alterTableWithPtoscRaw(
     knex,
-    'widgets',
-    (table) => {
-      table.renameColumn('title', 'name');
-    },
+    'ALTER TABLE widgets CHANGE COLUMN `title` `name` VARCHAR(255) NOT NULL',
     createPtoscOptions('rename_widgets_title_to_name')
   );
 };
